dd8c2b35139d20c1006cdca96616f5e0b9741fc9
sooham <rafizsooham@gmail.com>
2016-09-17 01:02:45 -0400

    Added OpenCV static libs and java files

--- app/src/main/java/rafiz/sooham/immerse/MainActivity.java
+++ app/src/main/java/rafiz/sooham/immerse/MainActivity.java
@@ -2,8 +2,11 @@ package rafiz.sooham.immerse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.opencv.core.Core;
 
 public class MainActivity extends AppCompatActivity {
+
+    static{ System.loadLibrary("opencv_java3"); }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
